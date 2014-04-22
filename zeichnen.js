d09b12f7c946d6879f6789769873931452982ca8
FHAux <katrin.riedelbauch@hs-augsburg.de>
2014-04-22 14:31:55 +0200

    Update zeichnen.js

--- zeichnen.js
+++ zeichnen.js
@@ -1,11 +1,26 @@
+/**
+ * @module    $cc$
+ * @author    Nicole Weiß
+ * @author    Katrin Riedelbauch
+ * @copyright 2014, Nicole Weiß, Katrin Riedelbauch
+ */
+
 (function($cc$, document)
 {
   "use strict";
+
+  /**
+   * @class
+   * @name  $cc$.game.zeichnen
+   * 
+   * @param {Object} main
+   *        Die Main-Klasse wird mitgegeben
+   */
   function zeichnen(main)
   {
     this.canvas = document.getElementById("zeichenflaeche");
     this.context = this.canvas.getContext("2d");
-    this.isDown = false; //ist die Maustaste noch gedrückt
+    this.isDown = false; // ist die Maustaste noch gedrückt
 
     // Mauslistener
     document.addEventListener("mousedown", (function(event)
@@ -22,7 +37,6 @@
       {
         var mousePos = this.getMousePos(event, this.canvas);
         this.moveOn(mousePos);
-        console.log(this);
         main.publishmousemove(mousePos);
       }
     }).bind(this));
@@ -31,31 +45,59 @@
     {
       this.isDown = false;
     }).bind(this));
-  }
-  ;
+  };
 
   zeichnen.prototype =
   {
-      // Gibt die Position der Maus relativ zum Canvas an
-      getMousePos: function(event, canvas) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-          x: event.clientX - rect.left,
-          y: event.clientY - rect.top
-        };
-      },
-      
-      // Beginne einen neuen Path
-      begin: function(mousePos) {
-        this.context.beginPath();
-        this.context.moveTo(mousePos.x, mousePos.y);
-      },
-      
-      // Zeichne den Path
-      moveOn: function(mousePos) {
-        this.context.lineTo(mousePos.x, mousePos.y);
-        this.context.stroke();
-      }
+    /**
+     * @method
+     * @name  $cc$.game.zeichnen#getMousePos
+     * 
+     * @param {Object} event
+     *        Enthält das von einem Mausklick erzeugte Event.
+     * @paran {Object} canvas
+     *        Enthält die Zeichenfläche
+     * 
+     * Gibt die Mausposition relativ zur Zeichenfläche wieder.
+     */
+    getMousePos : function(event, canvas)
+    {
+      var rect = canvas.getBoundingClientRect();
+      return {
+        x : event.clientX - rect.left,
+        y : event.clientY - rect.top
+      };
+    },
+
+    /**
+     * @method
+     * @name  $cc$.game.zeichnen#begin
+     * 
+     * @param {Object} mousePos
+     *        Enthält die Mausposition relativ zur Zeichenfläche.
+     * 
+     * Beginnt einen neuen Pfad.
+     */
+    begin : function(mousePos)
+    {
+      this.context.beginPath();
+      this.context.moveTo(mousePos.x, mousePos.y);
+    },
+
+    /**
+     * @method
+     * @name  $cc$.game.zeichnen#moveOn
+     * 
+     * @param {Object} mousePos
+     *        Enthält die Mausposition relativ zur Zeichenfläche.
+     * 
+     * Zeichnet den Pfad.
+     */
+    moveOn : function(mousePos)
+    {
+      this.context.lineTo(mousePos.x, mousePos.y);
+      this.context.stroke();
+    }
   };
 
   // Im Package platzieren
