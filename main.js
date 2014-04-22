385de94edce81c1d686b3bc91a1d4dedb99bd4f6
FHAux <katrin.riedelbauch@hs-augsburg.de>
2014-04-22 14:31:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-// Package definieren
-this.$cc$ = {game : {}};
+/**
+ * @module    $cc$
+ * @author    Nicole Weiß
+ * @author    Katrin Riedelbauch
+ * @copyright 2014, Nicole Weiß, Katrin Riedelbauch
+ */
 
-(function($cc$, window)
+// Package definieren
+this.$cc$ =
+{
+  game : {}
+};
+
+(function($cc$, window, document)
 {
   function main()
   {
+    /**
+     * @class
+     * @name $cc$.game.main
+     */
+
     // Zeichenklasse initialisieren
-    var zeichnen = new $cc$.game.zeichnen(this);
+    var zeichnen = new $cc$.game.zeichnen(this),
+        chat = new $cc$.game.chat(this);
 
     AUTOBAHN_DEBUG = true;
+    DEBUG = false;
 
     this.connection = new autobahn.Connection(
     {
@@ -19,23 +36,61 @@ this.$cc$ = {game : {}};
     // Wird ausgeführt wenn die Verbindung hergestellt ist
     this.connection.onopen = function(session)
     {
-
       console.log("session established!");
 
-      // Die Eventhändler
-      //
+      /**
+       * @method
+       * @name  $cc$.game.main#onmousedown
+       * 
+       * @param {Object} mousePos
+       *        Enthält die Mausposition relativ zur Zeichenfläche.
+       * 
+       * Wird ausgeführt, wenn auf einem anderen Client die Maustaste innerhalb
+       * der Zeichenfläche gedrückt wird und setzt im aktuellen Client an dieser
+       * Position ebenfalls einen neuen Pfad an.
+       */
       function onmousedown(mousePos)
       {
-        console.log(mousePos);
-        zeichnen.begin(mousePos);
+        if (DEBUG)
+        {
+          console.log(mousePos);
+        };
+        zeichnen.begin(mousePos[0]);
       };
 
+      /**
+       * @method
+       * @name  $cc$.game.main#onmousemove
+       * 
+       * @param {Object} mousePos
+       *        Enthält die Mausposition relativ zur Zeichenfläche.
+       * 
+       * Wird ausgeführt, wenn auf einem anderen Client die Maustaste innerhalb
+       * der Zeichenfläche gedrückt ist und die Maus bewegt wird und zeichnet
+       * den dort gezeichneten Pfad im aktuellen Client nach.
+       */
       function onmousemove(mousePos)
       {
-        console.log(mousePos);
-        console.log(zeichnen);
-        console.log(zeichnen.moveOn);
-        zeichnen.moveOn(mousePos);
+        if (DEBUG)
+        {
+          console.log(mousePos);
+        };
+        zeichnen.moveOn(mousePos[0]);
+      };
+
+      /**
+       * @method
+       * @name  $cc$.game.main#onsenden
+       * 
+       * @param {Object} eingabe
+       *        Enthält die Chateingabe
+       * 
+       * Wird ausgeführt wenn auf einem anderen Client eine Chateingabe gesendet
+       * wird und zeigt diese auch im aktuellen Client an.
+       */
+      function onsenden(eingabe)
+      {
+        chat.anzeigen(eingabe);
       };
 
       // Für die verschiedenen Themen registrieren
@@ -57,6 +112,16 @@ this.$cc$ = {game : {}};
           {
             console.log(error);
           });
+
+      session.subscribe('de.copycat.senden', onsenden).then(
+          function(subscription)
+          {
+            console.log("ok, subscribed with ID " + subscription.id);
+          }, function(error)
+          {
+            console.log(error);
+          });
+
     };
 
     // Wird ausgeführt wenn die Verbindung abbricht
@@ -67,39 +132,82 @@ this.$cc$ = {game : {}};
 
     // Öffnet die Verbindung
     this.connection.open();
-   
+
   }
 
   main.prototype =
   {
-    // Unsere Publishmethoden  
+    /**
+     * @method
+     * @name  $cc$.game.main#publishmousedown
+     * 
+     * @param {Object} mousePos
+     *        Enthält die Mausposition relativ zur Zeichenfläche.
+     * 
+     * Wird ausgeführt wenn die Maustaste innerhalb der Zeichenfläche gedrückt
+     * wird und überträgt die Mausposition zum Server.
+     */
     publishmousedown : function(mousePos)
     {
       if (this.connection.session)
       {
         this.connection.session.publish("de.copycat.mousedown",
-        [mousePos]);
+        [ mousePos ]);
       } else
       {
         console.log("cannot publish: no session");
       }
     },
 
+    /**
+     * @method
+     * @name  $cc$.game.main#publishmousemove
+     * 
+     * @param {Object} mousePos
+     *        Enthält die Mausposition relativ zur Zeichenfläche.
+     * 
+     * Wird ausgeführt wenn die Maustaste innerhalb der Zeichenfläche gedrückt
+     * ist und die Maus bewegt wird und überträgt die Mausposition zum Server.
+     */
     publishmousemove : function(mousePos)
     {
       if (this.connection.session)
       {
         this.connection.session.publish("de.copycat.mousemove",
-        [mousePos]);
+        [ mousePos ]);
       } else
       {
         console.log("cannot publish: no session");
       }
     },
 
-    
+    /**
+     * @method
+     * @name  $cc$.game.main#publishsenden
+     * 
+     * @param {Object} eingabe
+     *        Enthält die Chateingabe
+     * 
+     * Wird ausgeführt wenn die Chateingabe gesendet wird und überträgt die
+     * Eingabe an den Server.
+     */
+    publishsenden : function(eingabe)
+    {
+      if (this.connection.session)
+      {
+        this.connection.session.publish("de.copycat.senden",
+        [ eingabe ]);
+      } else
+      {
+        console.log("cannot publish: no session");
+      }
+    }
+
   };
 
-  window.onload = function() {new main();};
+  window.onload = function()
+  {
+    new main();
+  };
 
-}(this.$cc$, this.window));
+}(this.$cc$, this.window, this.document));
